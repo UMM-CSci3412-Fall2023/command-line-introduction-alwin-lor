15b125b7fbb6cd91b6e954e825d394f41e17ac1b
Alwin Lor <lor00148@morris.umn.edu>
2023-09-12 11:09:20 -0500

    add comments to extract_and_compile.sh comments added make it clear what each line does

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
-tar -kzxf NthPrime.tgz
-cd ./NthPrime
-gcc main.c nth_prime.c -o NthPrime ## not working yet
+tar -kzxf NthPrime.tgz ##unpackages and extracts the NthPrime.tgz file
+cd ./NthPrime || exit ##change directory to the newly created folder or exit should it not exist
+gcc main.c nth_prime.c -o NthPrime ## takes the two .c files in the folder and compiles and links them together
+                                   ## as the executable NthPrime
 
-./NthPrime $1
+./NthPrime "$1" ##runs the executable NthPrime with the given variable
